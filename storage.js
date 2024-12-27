1e13019bf2f73f6a0c80dd5722ccaf0b64e57b13
Byoo_ <36179767+XeKrOsu@users.noreply.github.com>
2024-12-27 22:41:23 +0100

    upload lpn-tracker

--- storage.js
+++ storage.js
@@ -1,13 +1,12 @@
 class Storage {
     static saveEmployees(employees) {
-        localStorage.setItem('employees', JSON.stringify(employees));
+        localStorage.setItem('lpn-tracker-employees', JSON.stringify(employees));
     }
 
     static loadEmployees() {
-        const storedEmployees = localStorage.getItem('employees');
+        const storedEmployees = localStorage.getItem('lpn-tracker-employees');
         if (storedEmployees) {
             const employees = JSON.parse(storedEmployees);
-            // Convertir les dates stockées en objets Date
             employees.forEach(emp => {
                 emp.startDate = new Date(emp.startDate);
                 emp.lastMissionDate = emp.lastMissionDate === 'N/A' ? 'N/A' : new Date(emp.lastMissionDate);
